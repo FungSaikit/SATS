ba27a3b595348d4df484acbc9f9815ea3f88423f
FungSaikit <675703302@qq.com>
2017-12-25 17:08:58 +0800

    12.25

---
+++ mail/MailConfig.js
@@ -0,0 +1,10 @@
+var mailConfig = {
+    service: 'qq',
+    port: 465, // SMTP 端口
+    secureConnection: true, // 使用 SSL
+    auth: {
+        user: '768065158@qq.com',
+        //这里密码不是qq密码，是你设置的smtp密码
+        pass: '*****'
+    }
+}
