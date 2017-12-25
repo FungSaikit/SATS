10aca9a1d3167c3333780719b60d582729d452d4
FungSaikit <675703302@qq.com>
2017-12-25 17:08:10 +0800

    Delete DBConfig.js

--- db/DBConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mysql: {
-        host: '47.93.57.54', 
-        user: 'root', 
-        password: 'JackFung1996', 
-        database: 'secondhand_apartment_trading_system', 
-        port: 3306
-    },
-}
