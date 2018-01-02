74a2b0563f68d9bda8b8a5f33169ec59e8fbcb5f
FungSaikit <675703302@qq.com>
2018-01-02 17:33:53 +0800

    update .gitignore

--- db/DBConfig.js
+++
@@ -1,9 +0,0 @@
-var dbConfig = {
-    host: '47.93.57.54',
-    user: 'root',
-    password: 'JackFung1996',
-    database: 'secondhand_apartment_trading_system',  
-    port: '3306'
-}
-
-module.exports = dbConfig;
